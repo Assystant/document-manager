5b1a68c1d96ed6ffeec862b9d7d9e2f49a2f00f2
shubham1-assystant <156311082+shubham1-assystant@users.noreply.github.com>
2024-03-26 08:10:54 +0530

    Update setup.py
    Signed-off-by: shubham1-assystant <156311082+shubham1-assystant@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name="doc_manager",
+    name="document-manager",
     version="0.0.1",
     author="Assystant Technologies Pvt Ltd",
     description="A simple document manager to store media files in S3 storage bucket.",
